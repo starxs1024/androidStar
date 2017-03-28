e372f432e902f80ae58b4a0bcccd19471470c02c
lyp <15616165649@163.com>
2017-03-28 23:25:59 +0800

    主线

---
+++ app/src/main/java/com/star/y/App.java
@@ -0,0 +1,33 @@
+package com.star.y;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.orhanobut.logger.LogLevel;
+import com.orhanobut.logger.Logger;
+import com.star.y.BuildConfig;
+
+/**
+ * Created by dongjunkun on 2016/2/1.
+ */
+public class App extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+
+        Logger.init("hhh")
+                .methodOffset(2)
+                .methodCount(2)
+                .hideThreadInfo()
+                .logLevel(BuildConfig.DEBUG ? LogLevel.FULL : LogLevel.NONE);
+
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
---
+++ app/src/main/java/com/star/y/event/SkinChangeEvent.java
@@ -0,0 +1,7 @@
+package com.star.y.event;
+
+/**
+ * Created by dongjunkun on 2016/2/19.
+ */
+public class SkinChangeEvent {
+}
